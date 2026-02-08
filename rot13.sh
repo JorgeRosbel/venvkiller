e78c173bc541fbe798327319405796b9cc44378e
JorgeRosbel <jorgerosbelcruzarias@gmail.com>
2026-02-08 17:08:52 +0000

    rot13

---
+++ rot13.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+rot13() {
+    if [ "$2" == "-d" ]; then
+        cat $1 | tr 'N-ZA-Mn-za-m' 'A-Za-z'
+    elif [ "$2" == "-e" ]; then
+        cat $1 | tr 'A-Za-z' 'N-ZA-Mn-za-m'
+    else
+        echo "Usage: rot13 <file> <option>"
+        echo "Options:"
+        echo "  -d  Decode"
+        echo "  -e  Encode"
+        exit 1
+    fi
+}
+
+rot13 $1 $2
+
